acaa33540e02a2bc6884bdb79f672d1aca3d1761
fatmahhelal <ana.fatim16@gmail.com>
2021-01-10 15:41:31 +0300

    change about us

--- src/component/About.js
+++ src/component/About.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-            <div>
-                <h1 color="red"> Aboutttt</h1>
-                
+            <div >
+                <main class="px-3">
+                    <h1>About US</h1>
+                    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Cover is a one-page template for buCover is a one-page template for buCover is a one-page template for buDownload, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                    <p class="lead">
+                        <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+                    </p>
+                </main>
             </div>
+
         )
     }
 }
